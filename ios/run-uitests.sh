74893416cd4e6f24aacd363158dc2af20eb28269
Matthew Zavislak <elevenfive@users.noreply.github.com>
2025-11-12 11:27:34 -0800

    Don't pin iOS simulator OS version

--- ios/run-uitests.sh
+++ ios/run-uitests.sh
@@ -14,7 +14,7 @@ xcodebuild test \
   -project acidwarp-ios.xcodeproj \
   -scheme acidwarpUITests \
   -configuration Release \
-  -destination "platform=iOS Simulator,arch=$ARCH,OS=26.1,name=iPhone 17 Pro" \
+  -destination "platform=iOS Simulator,arch=$ARCH,name=iPhone 17 Pro" \
   -parallel-testing-enabled NO \
   CODE_SIGN_IDENTITY="" \
   CODE_SIGNING_REQUIRED=NO \
